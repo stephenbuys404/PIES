d01443cf895bdc168b67ea28195aa5cbf207e27d
unknown <stephenbuys404@gmail.com>
2023-05-21 09:33:14 -0700

    Projects

---
+++ Stocks.py
@@ -0,0 +1,62 @@
+#pandas
+#plotly
+#yfinance
+import pandas
+import yfinance as yf
+import plotly.graph_objs as go
+
+#UBER       
+#TSLA       
+#MERC       
+#BMWYY      
+#VWAGY      
+#TM         
+#SSUN.F     
+#AAL      
+#AAP      
+#AAPL      
+#LOMAX   
+#AAT        
+#AES        
+#BBCA      
+#ELBM       
+#AMSC      
+#MULN     
+#AMZN      
+#MSFT    
+#BURU     
+#QLYS       
+#IBM       
+#COKE      
+#KO        
+#NKE       
+#DIS      
+#GE        
+#LPL       
+#VOD.JO     
+#VDMCY      
+#SPCB      
+#BTC-USD 
+#MCD       
+#BA        
+#ACN       
+#RYBIF
+
+ticker='IBM'
+fig:go._figure.Figure=go.Figure()
+data:pandas.core.frame.DataFrame=yf.download(tickers=ticker, period='1d', interval='1m')
+fig.add_trace(go.Candlestick(x=data.index,open=data['Open'],high=data['High'],low=data['Low'],close=data['Close'],name='market-data'))
+fig.update_layout(title='live share price evolution',yaxis_title='Stock Price(USD per Shares)')
+fig.update_xaxes(
+    rangeslider_visible=True,
+    rangeselector=dict(
+        buttons=list([
+            dict(count=5, label='5m', step='minute', stepmode='backward'),
+            dict(count=15, label='15m', step='minute', stepmode='backward'),
+            dict(count=45, label='45m', step='minute', stepmode='backward'),
+            dict(count=1, label='HTD', step='hour', stepmode='todate'),
+            dict(count=3, label='3h', step='hour', stepmode='backward'),
+            dict(count=5, label='5h', step='hour', stepmode='backward'),
+            dict(count=7, label='7h', step='hour', stepmode='backward'),
+            dict(step='all')])))
+fig.show()
