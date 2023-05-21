a699ade1cdb8fa5ea4bef7a35b21db4d1c65f17f
unknown <stephenbuys404@gmail.com>
2023-05-21 10:59:09 -0700

    Project

---
+++ PDF.py
@@ -0,0 +1,13 @@
+#pdfkit
+#https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox-0.12.6-1.msvc2015-win64.exe
+import os
+import pdfkit
+from pdfkit.api import configuration
+
+def convert(link,name):
+    filename='C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe'
+    if(os.path.exists(filename)):
+        wkhtml_path=pdfkit.configuration(wkhtmltopdf=filename)
+        pdfkit.from_url(link,name,configuration=wkhtml_path)
+
+convert('https://www.google.com','sample.pdf')
