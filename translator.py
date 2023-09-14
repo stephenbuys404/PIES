657dfaeb1c811238a16a363a1779fdf8cde4dda1
stephenbuys711 <114079563+stephenbuys711@users.noreply.github.com>
2023-09-14 09:30:01 +0200

    Update translator.py

--- translator.py
+++ translator.py
@@ -3,29 +3,28 @@ from tkinter import *
 from translate import Translator
 
 Screen = Tk()
-if(Screen):
-    InputLanguageChoice = StringVar()
-    TranslateLanguageChoice = StringVar()
-    LanguageChoices = {'English','French','German'}
-    def Translate():
-        translator = Translator(from_lang= InputLanguageChoice.get(),to_lang=TranslateLanguageChoice.get())
-        if(translator):
-            Translation = translator.translate(TextVar.get())
-            if(Translation):
-                OutputVar.set(Translation)
-    InputLanguageChoiceMenu = OptionMenu(Screen,InputLanguageChoice,*LanguageChoices)
-    if(InputLanguageChoiceMenu):
-        Label(Screen,text='Choose Language').grid(row=0,column=1)
-        InputLanguageChoiceMenu.grid(row=1,column=1)
-        NewLanguageChoiceMenu = OptionMenu(Screen,TranslateLanguageChoice,*LanguageChoices)
-        if(NewLanguageChoiceMenu):
-            Label(Screen,text='Translate').grid(row=0,column=2)
-            NewLanguageChoiceMenu.grid(row=1,column=2)
-            Label(Screen,text='Enter Sentence').grid(row=2,column =0)
-            TextVar = StringVar()
-            TextBox = Entry(Screen,textvariable=TextVar).grid(row=2,column = 1)
-            Label(Screen,text='Output').grid(row=2,column =2)
-            OutputVar = StringVar()
-            TextBox = Entry(Screen,textvariable=OutputVar).grid(row=2,column = 3)
-            Button(Screen,text='Translation',command=Translate, relief = GROOVE).grid(row=3,column=1,columnspan = 3)
-            mainloop()
+InputLanguageChoice = StringVar()
+TranslateLanguageChoice = StringVar()
+LanguageChoices = {'English','French','German'}
+
+def Translate():
+    translator = Translator(from_lang= InputLanguageChoice.get(),to_lang=TranslateLanguageChoice.get())
+    Translation = translator.translate(TextVar.get())
+    OutputVar.set(Translation)
+            
+InputLanguageChoiceMenu = OptionMenu(Screen,InputLanguageChoice,*LanguageChoices)
+Label(Screen,text='Choose Language').grid(row=0,column=1)
+InputLanguageChoiceMenu.grid(row=1,column=1)
+NewLanguageChoiceMenu = OptionMenu(Screen,TranslateLanguageChoice,*LanguageChoices)
+
+Label(Screen,text='Translate').grid(row=0,column=2)
+NewLanguageChoiceMenu.grid(row=1,column=2)
+Label(Screen,text='Enter Sentence').grid(row=2,column =0)
+TextVar = StringVar()
+TextBox = Entry(Screen,textvariable=TextVar).grid(row=2,column = 1)
+Label(Screen,text='Output').grid(row=2,column =2)
+OutputVar = StringVar()
+TextBox = Entry(Screen,textvariable=OutputVar).grid(row=2,column = 3)
+Button(Screen,text='Translation',command=Translate, relief = GROOVE).grid(row=3,column=1,columnspan = 3)
+
+mainloop()
