9f2ebb4ffc4a8a925b3d70cf5e2b18e11c53ffe4
stephenbuys711 <114079563+stephenbuys711@users.noreply.github.com>
2023-09-14 09:48:10 +0200

    Update Hashed.py

--- Hashed.py
+++ Hashed.py
@@ -1,25 +1,19 @@
 import hashlib
 
 hash_object = hashlib.md5(b'Hello World')
-if(not hash_object is None):
-    print(hash_object.hexdigest())
+print(hash_object.hexdigest())
 
 hash_object = hashlib.sha1(b'Hello World')
-if(not hash_object is None):
-    print(hash_object.hexdigest())
+print(hash_object.hexdigest())
 
 hash_object = hashlib.sha224(b'Hello World')
-if(not hash_object is None):
-    print(hash_object.hexdigest())
+print(hash_object.hexdigest())
 
 hash_object = hashlib.sha256(b'Hello World')
-if(not hash_object is None):
-    print(hash_object.hexdigest())
+print(hash_object.hexdigest())
 
 hash_object = hashlib.sha384(b'Hello World')
-if(not hash_object is None):
-    print(hash_object.hexdigest())
+print(hash_object.hexdigest())
 
 hash_object = hashlib.sha512(b'Hello World')
-if(not hash_object is None):
-    print(hash_object.hexdigest())
+print(hash_object.hexdigest())
