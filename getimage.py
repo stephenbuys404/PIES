e644be0175230e664ea1fa68459467ea798ca0c0
stephenbuys711 <114079563+stephenbuys711@users.noreply.github.com>
2023-09-14 09:17:51 +0200

    Update getimage.py

--- getimage.py
+++ getimage.py
@@ -7,8 +7,9 @@ def Save(url):
     print(f_ext)
     f_name = 'img{}'.format(f_ext)
     page = requests.get(url)
-    with open(f_name, 'wb') as f:
-        f.write(page.content)
+    f=open(f_name,'wb')
+    f.write(page.content)
+    f.close()
 
 url='https://tse3.mm.bing.net/th?id=OIP.U46kDcwtmtNDF6EZIenZtAHaE8&pid=15.1.jpg'
 Save(url)
