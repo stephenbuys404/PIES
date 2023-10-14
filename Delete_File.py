057a1111d6ff9ef4cf28073b2b5362e464839ab3
stephenbuys404 <114079563+stephenbuys404@users.noreply.github.com>
2023-10-14 19:49:37 +0200

    Delete Delete_File.py

--- Delete_File.py
+++
@@ -1,8 +0,0 @@
-import os
-from tkinter import filedialog as dialog
-
-file = dialog.askopenfilename()
-while(file!=''):
-    if(os.path.exists(file)):
-        os.remove(file)
-    file = dialog.askopenfilename()
